d3ec27140e3b036885573720c60fb6650efd9737
Aaron Sandgren <aaron.sandgren@live.se>
2021-10-18 04:07:07 +0200

    lamp

---
+++ src/components/skillcontainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+interface Props{
+
+}
+
+const SkillContainer: React.FC<Props> = ({}) => 
+  <div className="SkillContainer">
+    
+  </div>
+
+export default SkillContainer;
