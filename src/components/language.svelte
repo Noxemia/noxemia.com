10e282c48c1a5bf86adadc13412c1e92f09ef08c
Aaron Sandgren <aaron.sandgren@live.se>
2022-03-21 20:00:23 +0100

    update2

---
+++ src/components/language.svelte
@@ -0,0 +1,41 @@
+<script>
+    export let name
+    export let grade
+
+    if(grade == undefined){
+        grade = 3
+    }
+    let stars = " "
+    for(let i = 0; i < grade; i++){
+        stars+="* "
+    }
+</script>
+
+<div class="main">
+    <p class="name">{name}</p>
+    <div class="confidence">
+        Confidence: 
+        <div class="stars">
+            {stars}
+        </div>
+    </div>
+</div>
+
+<style>
+    .main{
+        width: 100%;
+        height: 15%;
+        border: solid 1px black;
+    }
+    .name{
+        font-family: "monospace", sans-serif;
+        font-size: 1.2em;
+        margin: 2px 0 0 5px;
+    }
+    .confidence{
+        display:flex;
+        width:100%;
+    }
+    .stars{margin-left: 2px;}
+
+</style>
---
+++ src/routes/cv.svelte
@@ -0,0 +1,36 @@
+<script>
+    import Language from "../components/language.svelte";
+</script>
+
+<div class="main">
+    <div class="temp"></div>
+    <div class="languages column"><p class="coltext">Programing Languages</p><Language name="Javascript" grade={4}/></div>
+    <div class="courses column"><p class="coltext">Courses</p></div>
+    
+</div>
+
+<style>
+    .main {
+        height: 80vh;
+        width: 100%;
+        display: flex;
+    }
+    .temp{
+        height: 100%;
+        width: 60%;
+    }
+    .column{
+        width: 20%;
+        height: 100%;
+        border: solid black 1px;
+        float: right;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+    }
+    .coltext{
+        margin-top: 4px;
+        font-family: "monospace", sans-serif;
+        font-size: 1.2em;
+    }
+</style>
